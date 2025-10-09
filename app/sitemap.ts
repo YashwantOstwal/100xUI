a1b42da4f86ca0ea9dfc4b21b1fb6200b1173977
Yashwant_Ostwal <yashwant08012005@gmail.com>
2025-10-09 18:36:17 +0530

    Updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,32 +3,38 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      url: `https://100xui.com`,
       lastModified: "2025-09-26",
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/components/parallax-cards`,
+      url: `https://100xui.com/components/parallax-cards`,
       lastModified: "2025-09-26",
       changeFrequency: "monthly",
       priority: 0.3,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/components/motion-dock`,
+      url: `https://100xui.com/components/motion-dock`,
       lastModified: "2025-09-26",
       changeFrequency: "monthly",
       priority: 0.3,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/components/text-switcher`,
+      url: `https://100xui.com/components/text-switcher`,
       lastModified: "2025-09-26",
       changeFrequency: "monthly",
       priority: 0.3,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/components/in-page-navbar`,
+      url: `https://100xui.com/components/in-page-navbar`,
       lastModified: "2025-09-26",
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
+    {
+      url: `https://100xui.com/components/spinning-testimonials`,
+      lastModified: "2025-10-09",
       changeFrequency: "monthly",
       priority: 0.3,
     },
